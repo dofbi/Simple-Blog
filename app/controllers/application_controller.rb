23dee931972d208fce678d1e7705e026e469b587
Mamadou Diagne <dofbi@powerbook-g4-17-de-mamadou-diagne.local>
2010-02-17 02:40:45 +0000

    Ajout d'une gestion de session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,6 @@ class ApplicationController < ActionController::Base
 
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
+  
+  #session :session_key =>'_simple_blog'
 end
