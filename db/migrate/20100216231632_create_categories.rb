6112672abd5af7d255c7a33b4face667711d7d59
Mamadou Diagne <dofbi@powerbook-g4-17-de-mamadou-diagne.local>
2010-02-17 00:37:53 +0000

    Model categorie

---
+++ db/migrate/20100216231632_create_categories.rb
@@ -0,0 +1,24 @@
+class CreateCategories < ActiveRecord::Migration
+  def self.up
+    create_table :categories do |t|
+      t.string :name,        :limit => 50, :default=> "", :null => false
+      t.string :short_name,  :limit => 30, :default=> "", :null => false
+      t.string :description,               :default=> "", :null => false
+    end
+    
+    create_table :categories_posts, :id => false do |t|
+      t.integer :category_id, :null => false
+      t.integer :post_id, :nnull => false
+    end
+    
+    add_index :categories_posts, :category_id
+    add_index :categories_posts, :post_id
+    remove_column :blog_posts, :category
+  end
+
+  def self.down
+    drop_table :categories
+    drop_table :categories_posts
+    add_column :blog_pots, :category, :string, :limit => 20, :default => "", :null => false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,19 +9,32 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100216224321) do
+ActiveRecord::Schema.define(:version => 20100217002002) do
 
   create_table "blog_posts", :force => true do |t|
     t.string   "title",      :limit => 100, :default => "", :null => false
     t.text     "content",                   :default => "", :null => false
     t.integer  "author_id",  :limit => 100, :default => 0,  :null => false
-    t.string   "category",   :limit => 20,  :default => "", :null => false
     t.string   "status",     :limit => 20,  :default => "", :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
   add_index "blog_posts", ["author_id"], :name => "index_blog_posts_on_author_id"
+
+  create_table "categories", :force => true do |t|
+    t.string "name",        :limit => 50, :default => "", :null => false
+    t.string "short_name",  :limit => 30, :default => "", :null => false
+    t.string "description",               :default => "", :null => false
+  end
+
+  create_table "categories_posts", :force => true do |t|
+    t.integer "category_id", :null => false
+    t.integer "post_id"
+  end
+
+  add_index "categories_posts", ["category_id"], :name => "index_categories_posts_on_category_id"
+  add_index "categories_posts", ["post_id"], :name => "index_categories_posts_on_post_id"
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id",                    :default => 0,  :null => false
