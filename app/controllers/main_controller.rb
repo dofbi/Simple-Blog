4887be652519b8ec93c446b9fb90e28577901048
Mamadou Diagne <genova@dofbi.com>
2010-02-27 07:53:31 +0000

    Ajout controleur et creation layout et css

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,17 @@
+class MainController < ApplicationController
+  def index
+  end
+
+  def list
+  end
+
+  def category
+  end
+
+  def archive
+  end
+
+  def view_post
+  end
+
+end
---
+++ app/controllers/staff_controller.rb
@@ -0,0 +1,17 @@
+class StaffController < ApplicationController
+	
+  layout "staff"
+  
+  def index
+  end
+
+  def login
+  end
+
+  def menu
+  end
+
+  def logout
+  end
+
+end
